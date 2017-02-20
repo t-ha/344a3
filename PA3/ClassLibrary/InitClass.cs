c27e7eced269c09a3482f9ff153fb514befb0d70
Tim Ha <tjunkha@gmail.com>
2017-02-20 05:51:42 -0800

    re

---
+++ PA3/ClassLibrary/InitClass.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure;
+
+namespace ClassLibrary
+{
+    public class InitClass
+    {
+        CloudQueue urlQueue;
+        CloudQueue adminQueue;
+        CloudTable titlesTable;
+
+        public InitClass()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            urlQueue = queueClient.GetQueueReference("urlqueue");
+            urlQueue.CreateIfNotExists();
+            adminQueue = queueClient.GetQueueReference("adminqueue");
+            adminQueue.CreateIfNotExists();
+
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            titlesTable = tableClient.GetTableReference("titlestable");
+            titlesTable.CreateIfNotExists();
+        }
+    }
+}
---
+++ PA3/WebRole1/admin.asmx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using ClassLibrary;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Diagnostics;
+using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Configuration;
+
+namespace WebRole1
+{
+    /// <summary>
+    /// Summary description for admin
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+
+    [System.Web.Script.Services.ScriptService]
+    public class admin : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string StartCrawling()
+        {
+            InitClass ic = new InitClass();
+            //CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings["StorageConnectionString"]);
+            //CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+            //CloudQueue urlQueue = queueClient.GetQueueReference("urlqueue");
+            //urlQueue.CreateIfNotExists();
+            string temp = "you started crawling!";
+            return new JavaScriptSerializer().Serialize(temp);
+        }
+
+        [WebMethod]
+        public void StopCrawling()
+        {
+
+        }
+
+        [WebMethod]
+        public void ClearIndex()
+        {
+
+        }
+
+        [WebMethod]
+        public void GetPageTitle()
+        {
+
+        }
+    }
+}
